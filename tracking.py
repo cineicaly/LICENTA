a5be73393647e6aaa9834384d78e00e40eba016d
Darius-Andrei Cipleu <darius.cipleu@student.upt.ro>
2024-06-07 15:09:11 +0300

    movement detection w/ background subtraction

--- tracking.py
+++ tracking.py
@@ -1,14 +1,35 @@
 import cv2
 import numpy as np
+def getVideoInfo(path):
+    path = '/demo/vehicles.mp4'
 
-cam = cv2.VideoCapture(0)
+# Load camera
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cam.read()
-    cv2.imshow('frame', frame)
+    # Get frames in realtime from the camera
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Create a Mask with adaptive threshold
+    mask = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 19, 5)
+
+    # Find contours
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 2000:
+            (x, y, w, h) = cv2.boundingRect(cnt)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    # Show frame and mask
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Gray", gray)
+    cv2.imshow("mask", mask)
+
     key = cv2.waitKey(1)
-    if key == 27: # key 27 = escape key
+    if key == 27:
         break
 
-cam.release()
+cap.release()
 cv2.destroyAllWindows()
